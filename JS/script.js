cdbef920162ffb0ee8701504b321bd3a064fe5c8
JonasLiljesand <jonas.liljesand@gmail.com>
2023-12-19 11:56:27 +0100

    arbete med fetch

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,13 @@
+import { sample } from "./pokemonSample.js"
+
+
+
 const teamViewbtn = document.getElementById('teamViewButton')
 const searchViewBtn = document.getElementById('searchViewButton')
 const viewTeamSec = document.getElementById('activeTeamSection')
 const viewSearchAndBenchSec = document.getElementById('searchAndBench')
+let pokedex = document.getElementById('pokedex')
+let search = ''
 
 teamViewbtn.addEventListener("click", () => {
 	viewSearchAndBenchSec.style.display = 'none'
@@ -11,4 +17,26 @@ teamViewbtn.addEventListener("click", () => {
 searchViewBtn.addEventListener("click", () => {
 	viewSearchAndBenchSec.style.display = 'block'
 	viewTeamSec.style.display = 'none'
-})
+
+
+async function searchPokemon() {
+	// const Name = document.getElementById('pokedex')
+
+	const resp = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+
+
+console.log(resp.value);
+
+}
+
+searchPokemon()
+
+
+	})
+
+
+function match() {
+	const pokeMatch = sample.results.name.filter(x => x.toLowerCase().includes(search))
+	console.log(pokeMatch);
+
+}
