ccc2705464557f724a4b17b5755c0f39c87d2c4c
JonasLiljesand <jonas.liljesand@gmail.com>
2023-12-24 01:06:24 +0100

    commit

--- JS/script.js
+++ JS/script.js
@@ -8,17 +8,18 @@ const reservViewBtn = document.getElementById('reservButton')
 const viewTeamSec = document.getElementById('activeTeamSection')
 const viewSearchAndBenchSec = document.getElementById('searchAndBench')
 const viewReservSec = document.getElementById('reservSec')
-const pokemonDataTemp =document.getElementById('pokemonDataTemp')
 const searchField = document.getElementById('pokemonSearchByName')
+// const nameField = document.getElementById('nameField')
 const resultsDiv = document.getElementById("resultsDiv");
+const active1 = document.getElementById('active1')
 let pokedex = document.getElementById('pokemonName')
 const reservList = []
-const teamList []
+const teamList = []
 let search = ''
 
 let pokemon = []
 let filterName = []
-const reservList = []
+
 
 teamViewbtn.addEventListener("click", () => {
 	viewSearchAndBenchSec.style.display = 'none'
@@ -39,18 +40,86 @@ reservViewBtn.addEventListener("click", () => {
 
 
 
+// 	async function fetchPokemonData() {
+//   try {
+//     const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
+//     const data = await response.json();
+
+//     const pokemonList = data.results;
+
+//     const pokemonData = await Promise.all(pokemonList.map(async (pokemon) => {
+//       const response = await fetch(pokemon.url);
+//       const pokemonDetails = await response.json();
+
+//       return {
+//         name: pokemonDetails.name,
+//         image: pokemonDetails.sprites.front_default,
+//       };
+//     }));
+// 	   console.log(pokemonData);
+//   } catch (error) {
+//     console.error('Error fetching data', error);
+//   }
+// }
 
 
 
-fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
-      .then(resp => resp.json())
-	  .then(data => {
-		pokemon = data.results.map(pokemon => {
-		return {name: pokemon.name}
+// fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+//       .then(resp => resp.json())
+// 	  .then(data => {
+// 		pokemon = data.results.map(pokemon => {
+// 		return {name: pokemon.name, id: pokemon.id}
+// 		// return {image: pokemon.sprites.front_default}
 
-		})
-	  })
-	  .catch(error => console.error('error fetching data', error))
+// 		})
+
+// 	//  pokemon = data.results.map(pokemon => ({
+//     //   name: pokemon.name,
+//     //   image: pokemon.sprites.front_default
+//     // 	}));
+// 		console.log(pokemon.length);
+// 	  })
+// 	  .catch(error => console.error('error fetching data', error))
+
+
+async function fetchPokemonData() {
+    try {
+        const resp = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0");
+        const data = await resp.json();
+
+        const pokePromises = data.results.map(async pokemon => {
+            const pokemonResp = await fetch(pokemon.url);
+            const pokemon = await pokemonResp.json();
+
+            return {
+                name: pokemon.name,
+                id: pokemon.id,
+                // image: pokemon.sprites.front_default
+            };
+        });
+
+        const pokemonData = await Promise.all(pokePromises);
+        return pokemonData;
+    } catch (error) {
+        console.error('error fetching data', error);
+    }
+}
+
+async function getPokemon() {
+    try {
+        const pokemonData = await fetchPokemonData();
+        pokemon = pokemonData;
+        console.log(pokemon.length);
+    } catch (error) {
+        console.error('error getting Pokemon', error);
+    }
+}
+
+getPokemon();
+
+
+
+
 
 searchField.addEventListener("input", e => {
 	const value = e.target.value.toLowerCase();
@@ -60,8 +129,20 @@ searchField.addEventListener("input", e => {
 	//TODO: begränsa antalet matchningar till ~10-15
 	filterName.forEach(pokemon => {
 		const pokeListItem = document.createElement('div');
-		pokeListItem.textContent = pokemon.name;
+		pokeListItem.textContent = pokemon.name + pokemon.id;
+
+		// const pokeImage = document.createElement('img')
+		// pokeImage = pokemon.image
+		const nameField = document.createElement('input',)
+		nameField.placeholder = `Namnge din ${pokemon.name}`
+		const addBtn = document.createElement('button')
+		addBtn.textContent = 'lägg till i lag'
+
+		// pokeListItem.appendChild(pokeImage)
+		pokeListItem.appendChild(nameField)
+		pokeListItem.appendChild(addBtn)
 		resultsDiv.appendChild(pokeListItem)
+
 	})
 
 })
@@ -69,10 +150,25 @@ searchField.addEventListener("input", e => {
 
 console.log("Filter Name Length:", filterName.length);
 searchField.addEventListener("keydown", enter => {
-	if(enter.key === "Enter") {
-		console.log("hej");
-		teamList.push(filterName[0])
-		console.log(teamList[0].name);
+	if(enter.key === "Enter" && filterName.length === 1) {
+		if(nameField.value !== '' ) {
+			filterName[0].nameField = nameField.value;
+		}
+		// console.log("test");
+		teamList.push(filterName)
+		console.log(teamList.length);
+		// console.log(teamList[0].nameField)
+		// console.log([]);
+		addToTeam()
 	}
 
 })
+
+
+
+
+// function addToTeam() {
+// 	if(active1.innerHTML === '' ) {
+// 		document.getElementById('active1').innerHTML = teamList[0].name
+// 	}
+// }
